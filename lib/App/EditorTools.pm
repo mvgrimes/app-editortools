95e05ad7591afc22ff2867233f664ed8b05dad95
Mark Grimes <mgrimes@cpan.org>
2009-06-24 14:28:45 -0700

    Updated to prepare for release to CPAN

--- lib/App/EditorTools.pm
+++ lib/App/EditorTools.pm
@@ -7,4 +7,45 @@ use App::Cmd::Setup -app;
 
 our $VERSION = 0.02;
 
+=pod
+
+=head1 NAME
+
+App::EditorTools - Command line tools to leverage PPIx::EditorTools (and maybe more)
+
+=head1 SYNOPSIS
+
+    See PPIx::EditorTools::* 
+
+=head1 DESCRIPTION
+
+Command line tools to leverage PPIx::EditorTools (and maybe more) for 
+editors that need the command line go between.
+
+=cut
+
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<PPIx::EditorTools>, L<Padre>
+
+=head1 BUGS
+
+Please report any bugs or suggestions at 
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-EditorTools>
+
+=head1 AUTHOR
+
+Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Mark Grimes
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
